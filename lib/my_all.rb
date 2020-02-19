b2c385b11ca142f94d3192c68c6435339f5ce66a
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-19 04:49:15 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,4 +9,11 @@ def my_all?(collection)
   yield(collection[i])
   i += 1
   end
+  
+  if
+    block_return_values.include?(false)
+                  false
+                else
+                  true
+end
 end
